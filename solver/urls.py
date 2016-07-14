54389af2d185705cfb2d62746402b87d3cefa959
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2016-07-13 23:57:45 -0400

    Changes: - Added /solver/numbers to urls - Created input_numbers view - Created static folder - Created main.css stylesheet - Added STATICFILES_DIRS to settings.py - Created base template (base.html) - Modified home.html and choose_method.html to extend from base.html - Created input_numbers.html template

--- solver/urls.py
+++ solver/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views as solver_views
 
 urlpatterns = [
-    url(r'^', solver_views.choose_method),
+    url(r'^$', solver_views.choose_method),
+    url(r'numbers/', solver_views.input_numbers),
 ]
--- solver/views.py
+++ solver/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def choose_method(request):
     return render(request, 'choose_method.html')
+
+def input_numbers(request):
+    return render(request, 'input_numbers.html')
