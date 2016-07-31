2afaee7a4e2774157be97ccaaa804f4425a5e481
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2016-07-31 19:34:28 -0400

    Modified main.css to remove scroll from number input, adjust grid width and number_slot properties. Added code in main.js to focus number input on element render. Modified base.html to add bootstrap

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,16 @@
 // console.log("REACT SCRIPT");
 
 var NumberInput = React.createClass({
-    render: function(){
-        return (
-            <div style={{fontSize: 30 + "px", position: "absolute"}}>
-              <input type="number" className="number_input" min="1" max="9"/>
-            </div>
-        );
-    }
+  componentDidMount: function(){
+    ReactDOM.findDOMNode(this.refs.number_input).focus(); 
+  },
+  render: function(){
+    return (
+      <div style={{fontSize: 30 + "px", position: "absolute"}}>
+        <input type="number" ref="number_input" className="number_input form-control" min="1" max="9"/>
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(
