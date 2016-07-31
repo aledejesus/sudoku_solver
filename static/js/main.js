480732dacee854b20f4c704ee1132d554aef9d3b
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2016-07-30 20:53:46 -0400

    Modified set_id script to set IDs with JS instead of with jQuery

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,16 @@
-// FIX SPACING ADDED WHEN ADDING NUMBERINPUT
+// console.log("REACT SCRIPT");
 
-// var NumberInput = React.createClass({
-//     render: function(){
-//         return (
-//             <div style={{fontSize: 10 + "px"}}>
-//               <input type="number" className="number_input" min="1" max="9"/>
-//             </div>
-//         );
-//     }
-// });
-//
-// ReactDOM.render(
-//   <NumberInput/>,
-//   document.getElementById('0_00')
-// );
+var NumberInput = React.createClass({
+    render: function(){
+        return (
+            <div style={{fontSize: 30 + "px", position: "absolute"}}>
+              <input type="number" className="number_input" min="1" max="9"/>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+  <NumberInput/>,
+  document.getElementById('0_00')
+);
