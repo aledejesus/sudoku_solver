96af81010edc05607758437f21d496f30ecb8c57
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2016-12-11 18:47:27 -0400

    Started creating factories and unit tests  - Created solver/factories.py file  - Created SudokuPuzzle factory  - Created SudokuPuzzle test case  - Created unit test for get_row method

--- solver/tests.py
+++ solver/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from factories import SudokuPuzzleFactory
+import numpy as np
 
-# Create your tests here.
+
+class SudokuPuzzleTestCase(TestCase):
+    def setUp(self):
+        self.puzzle = SudokuPuzzleFactory()
+
+    def test_get_row(self):
+        self.puzzle.solved_puzzle = self.puzzle.unsolved_puzzle
+        expected_row = [7, 4, 2, 8]
+        actual_row = self.puzzle.get_row(0)
+        self.assertTrue(np.array_equal(expected_row, actual_row))
