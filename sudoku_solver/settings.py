2d6d8765bdd8d95e83165d4090ddf261f0efaa5c
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2016-12-23 07:29:24 -0400

    Modified default databse password

--- sudoku_solver/settings.py
+++ sudoku_solver/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ss_db',
         'USER': 'sudoku_solver',
-        'PASSWORD': 'alesudoku',
+        'PASSWORD': '',
     }
 }
 
