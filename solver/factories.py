4e3c72a47862091453f8cb9d979bc9f62f7952c9
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2017-01-02 23:47:18 -0400

    Fixed list referencing bugs  - Modified SudokuPuzzle's solved_puzzle and missing_vals_pos default values to avoid referencing the same list in different instances  - Modified PuzzleCell's possibilities default value for the same reason as above  - Modified SudokuPuzzle's solve method to avoid referencing the same list in unsolved_puzzle and solved_puzzle  - Modified SudopuPuzzleFactory to clone lists  - Created get_empty_list and clone_list functions in solver/utils.py

--- solver/factories.py
+++ solver/factories.py
@@ -1,16 +1,17 @@
 import factory
 from . import models
+from sudoku_solver import utils
 
 EASY_PUZZLE = (
-    [0, 0, 0, 7, 0, 0, 4, 2, 8],
-    [7, 1, 4, 8, 0, 0, 0, 5, 0],
-    [2, 0, 0, 9, 4, 0, 0, 1, 0],
-    [8, 9, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 2, 0, 6, 0, 7, 0, 0],
-    [6, 0, 0, 0, 0, 0, 0, 4, 9],
-    [0, 2, 0, 0, 8, 4, 0, 0, 5],
-    [0, 5, 0, 0, 0, 2, 1, 7, 4],
-    [4, 6, 9, 0, 0, 7, 0, 0, 0]
+    (0, 0, 0, 7, 0, 0, 4, 2, 8),
+    (7, 1, 4, 8, 0, 0, 0, 5, 0),
+    (2, 0, 0, 9, 4, 0, 0, 1, 0),
+    (8, 9, 0, 0, 0, 0, 0, 0, 1),
+    (0, 0, 2, 0, 6, 0, 7, 0, 0),
+    (6, 0, 0, 0, 0, 0, 0, 4, 9),
+    (0, 2, 0, 0, 8, 4, 0, 0, 5),
+    (0, 5, 0, 0, 0, 2, 1, 7, 4),
+    (4, 6, 9, 0, 0, 7, 0, 0, 0)
 )
 
 
@@ -18,16 +19,9 @@ class SudokuPuzzleFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.SudokuPuzzle
 
-    unsolved_puzzle = list(EASY_PUZZLE)
+    unsolved_puzzle = utils.clone_list(list(EASY_PUZZLE))
     solved_puzzle = factory.LazyAttribute(
-        lambda obj: list(obj.unsolved_puzzle))
-
-    # @classmethod
-    # def _create(cls, model_class, *args, **kwargs):
-    #     """Create an instance of the model, and save it to the database."""
-    #     manager = cls._get_manager(model_class)
-    #     import pdb; pdb.set_trace()
-    #     return manager.create(*args, **kwargs)
+        lambda obj: utils.clone_list(obj.unsolved_puzzle))
 
 
 class PuzzleCellFactory(factory.django.DjangoModelFactory):
--- sudoku_solver/utils.py
+++ sudoku_solver/utils.py
@@ -1,3 +1,16 @@
 def remove_zeroes(arr):
     # removes zeroes from arr
     return filter(lambda x: x != 0, arr)
+
+
+def get_empty_list():
+    return list()
+
+
+def clone_list(original):
+    cloned = get_empty_list()
+
+    for i in range(len(original)):
+        cloned.append(list(original[i]))
+
+    return cloned
