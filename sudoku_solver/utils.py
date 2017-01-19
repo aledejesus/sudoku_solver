962ba099da6ba166bb3bb0c4417aaf28d8db16c2
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2017-01-18 22:32:09 -0400

    Deleted clone_list function

--- sudoku_solver/utils.py
+++ sudoku_solver/utils.py
@@ -1,15 +1,3 @@
 def remove_zeroes(arr):
     # removes zeroes from arr
     return filter(lambda x: x != 0, arr)
-
-
-def clone_list(original, contains_lists):
-    cloned = list()
-
-    if contains_lists:
-        for lst in original:
-            cloned.append(list(lst))
-    else:
-        cloned = list(original)
-
-    return cloned
