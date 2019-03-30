da503d583964f521cd47bd87b9a438d3a4f876a8
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2019-03-30 01:03:35 -0400

    Remove easy test from test_solver

--- solver/views.py
+++ solver/views.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 import numpy as np
 from silk.profiling.profiler import silk_profile
 
-TESTS = ('easy', 'medium', 'hard')
+TESTS = ('medium', 'hard')
 
 
 def choose_method(request):
